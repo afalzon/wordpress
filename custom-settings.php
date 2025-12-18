0654f69f0b844df3dc789f29e8f67bc1824d5498
afalzon <46345304+afalzon@users.noreply.github.com>
2025-12-18 15:31:41 +1100

    Update custom-settings.php

--- custom-settings.php
+++ custom-settings.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Aaron Falzon - Custom Theme Settings
  * Description: A custom settings panel for the theme with Front-end and Back-end configurations.
- * Version: 1.8
+ * Version: 1.8.2
  * Author: Aaron Falzon
  */
 
